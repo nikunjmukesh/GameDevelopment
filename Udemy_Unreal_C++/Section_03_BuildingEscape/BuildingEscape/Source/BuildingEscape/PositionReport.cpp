444e3366e3850ae488b65a9a22b8c83456bc9013
NIKUNJ-PC\Nikunj <nikunjmukesh@gmail.com>
2018-01-18 20:20:35 -0700

    door close/open functionilty added

--- Udemy_Unreal_C++/Section_03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ Udemy_Unreal_C++/Section_03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -10,7 +11,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -18,8 +18,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	
+	FName ObjectName = GetOwner()->GetFName();
+	FVector ObjectPos = GetOwner()->GetTransform().GetLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName.ToString(), *ObjectPos.ToString() );
 	
 }
 
